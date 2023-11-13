5290be98d95d2bd247f06731f74d8ae820cfd5ea
ChrisOosthuizen <chrisoosthuizen@ChrisOosthuizens-MacBook-Air.local>
2023-11-13 09:07:10 -0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './NavBar';
 function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-    Hello world!
+    <Navbar/>
   </h1>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'uh-red': '#eb3b5c',
+        'uh-white': '#ffffff',
+      },
+    },
   },
   plugins: [],
 }
-
